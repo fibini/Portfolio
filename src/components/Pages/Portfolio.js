66051337da5d9db43793ad19d4d1c99d1d5a91fd
Fabien <fabienswift@gmail.com>
2022-08-04 16:16:31 -0400

    Add portfolio page with project images

--- src/components/Pages/Portfolio.js
+++ src/components/Pages/Portfolio.js
@@ -1,10 +1,37 @@
-import React from 'react';
-import Mathproject from '../Project';
+/* eslint-disable react/jsx-key */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable react/jsx-props-no-spreading */
+import { React, useState } from 'react';
+import Slider from 'react-slick';
+import Games from '../../images/Games-store.png';
+import Book from '../../images/Book-store.png';
+import Maths from '../../images/Maths.png';
+import '../../CSS/portfolio.css';
 
-const Portfolio = () => (
-  <div>
-    <Mathproject />
-  </div>
-);
+const images = [Games, Book, Maths];
+
+const Portfolio = () => {
+  const [imageIndex, setImageIndex] = useState(0);
+
+  const settings = {
+    infinite: true,
+    lazyLoad: true,
+    speed: 300,
+    slidesToShow: 1,
+    centerMode: true,
+    centerPadding: 0,
+    beforeChange: (current, next) => setImageIndex(next),
+  };
+  return (
+    <div className="slides">
+      <Slider {...settings}>
+        {images.map((img, index) => (
+          <div className={index === imageIndex ? 'slide activeSlide' : 'slide'}><img src={img} alt={img} /></div>
+        ))}
+      </Slider>
+    </div>
+  );
+};
 
 export default Portfolio;
---
+++ src/components/Projecttwo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Games from '../images/Games-store.png';
+import '../CSS/maths.css';
+
+const Gamesproject = () => (
+  <div>
+    <a href="https://warm-tarsier-5ebf9e.netlify.app/"><img className="maths" src={Games} alt="Games project" /></a>
+    <a href="https://github.com/fibini/React-Redux-capstone"><h3>GITHUB REPOSITORY</h3></a>
+  </div>
+);
+
+export default Gamesproject;

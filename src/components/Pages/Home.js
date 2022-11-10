fe5bbf487f6c1735ee6b9f2863749b49c660eaea
Fabien <fabienswift@gmail.com>
2022-11-10 17:31:54 -0500

    Change background and add animation to home

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -5,11 +5,12 @@ import 'aos/dist/aos.css';
 
 export default function Home() {
   useEffect(() => {
-    Aos.init({ duration: 2000 });
+    Aos.init({ duration: 2500 });
   }, []);
 
   return (
     <div className="home-container">
+      <img src="http://unblast.com/wp-content/uploads/2021/01/Space-Background-Image-2.jpg" alt="space" className="bgimg" data-aos="zoom-in" />
       <div className="welcome" data-aos="fade-down">
         <h1>WELCOME</h1>
       </div>
